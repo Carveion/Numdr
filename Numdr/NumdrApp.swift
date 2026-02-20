061eb40b5d7632f4f760eb1ddf89e1247c9e7382
Karthik <karthikeyanelambharathi@gmail.com>
2026-02-20 16:06:50 +0530

    Initial Commit

---
+++ Numdr/NumdrApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumdrApp.swift
+//  Numdr
+//
+//  Created by Karthikeyan Elambharathi on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct NumdrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
